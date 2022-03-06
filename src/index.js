64ed9416f7f7b3e6b562d8294d687a7b784d6a97
Eduardo Tonatto <edtonatto@gmail.com>
2022-03-06 20:50:38 -0300

    :sparkles: Validação se usuário pode criar novo to-do
    Validação se usuário pode ou não criar um novo to-do, se usuário for Pro poderá criar ilimitados, caso não seja Pro e tenha menos de 10 to-dos criados então pode criar, em todos outros casos deve ser bloqueado

--- src/index.js
+++ src/index.js
@@ -21,7 +21,12 @@ function checksExistsUserAccount(request, response, next) {
 }
 
 function checksCreateTodosUserAvailability(request, response, next) {
-  // Complete aqui
+	const {user} = request
+
+	if(!user.pro && user.todos.length >= 10)
+		return response.status(403).json({error: "User does not have pro account and already have 10 todos."})
+
+	return next()
 }
 
 function checksTodoExists(request, response, next) {
